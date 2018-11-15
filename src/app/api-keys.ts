5be7263880c5194d7b5f3d3b11a799675f37697b
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2018-11-15 07:15:54 -0800

    include api-keys

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyDWfKh3LDukiBluYiM7XEZ3AzL7c4h5kjc",
+    authDomain: "nordstrom-rack.firebaseapp.com",
+    databaseURL: "https://nordstrom-rack.firebaseio.com",
+    projectId: "nordstrom-rack",
+    storageBucket: "nordstrom-rack.appspot.com",
+    messagingSenderId: "136465842991"
+}
